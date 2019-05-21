5c1fc59f11078d6d44593e7c0446a059599ba09d
Aditya Joshi <adi.joshi194@gmail.com>
2019-05-21 19:19:13 +0530

    Added New methods in Methods.java

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -1,5 +1,7 @@
 package base;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
@@ -7,5 +9,6 @@ public class BaseClass {
 
 	public WebDriver driver;
 	public EventFiringWebDriver eDriver;
+	public final Logger log = LogManager.getLogger("Log");
 
 }
--- src/main/java/utils/Methods.java
+++ src/main/java/utils/Methods.java
@@ -1,15 +1,26 @@
 package utils;
 
+import org.jboss.aerogear.security.otp.Totp;
 import org.openqa.selenium.By;
 
 import base.BaseClass;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Methods {
 
 	private BaseClass base;
+
+	private WebDriverWait wait;
 	
 	public Methods(BaseClass base) {
 		this.base = base;
+		wait = new WebDriverWait(base.eDriver, 10);
 	}
 	
 	public void navigateTo(String url) {
@@ -22,5 +33,35 @@ public class Methods {
 	
 	public void input(By element,String value) {
 		base.eDriver.findElement(element).sendKeys(value);
+	}
+
+	public void waitForVisible(By by) {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+	}
+
+	public String getAttributeValue(By by, String attribute) {
+		return base.eDriver.findElement(by).getAttribute(attribute);
+	}
+
+	public void openANewTab() {
+		((JavascriptExecutor)base.eDriver).executeScript("window.open()");
+		ArrayList<String> tabs = new ArrayList<String>(base.eDriver.getWindowHandles());
+		base.eDriver.switchTo().window(tabs.get(1));
+	}
+
+	public String generateTotp(String key) {
+		Totp top = null;
+		try {
+			top = new Totp(key);
+		}
+		catch(Exception ex) {
+			base.log.debug(ex);
+		}
+		return top.now();
+	}
+
+	public boolean webElementPresence(By by) {
+		List<WebElement> element = base.eDriver.findElements(by);
+		return !element.isEmpty();
 	}
 }
